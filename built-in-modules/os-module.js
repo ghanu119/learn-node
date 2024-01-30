8570c9ad9b803f2850abba7716c9186ea62eb044
gashra-aecor <gashra@aecordigital.com>
2024-01-30 17:36:33 +0530

    OS module

---
+++ built-in-modules/os-module.js
@@ -0,0 +1,17 @@
+const os = require('os');
+
+
+// Current user information
+console.log( os.userInfo() );
+
+// System Information
+console.log( os.uptime());
+
+// Home Dir
+console.log( os.homedir());
+
+// Hostname
+console.log( os.hostname());
+
+// platform
+console.log( os.platform());
