df245c2e358f7b6b8023bbc5aeab84a793d16a1f
ghanu119 <ghanshyamashra@gmail.com>
2024-02-24 12:01:22 +0530

    html load using express static

--- express-server/express-server-theme/server.js
+++ express-server/express-server-theme/server.js
@@ -5,20 +5,23 @@ const { readFileSync } = require('fs');
 server.use( '/assets',express.static( __dirname + '/assets'));
 
 server.listen( 5000, () => {
-    console.log( 'Server is listenig on 5000 port...');
+    console.log( 'Server is listenig on http://localhost:5000 port...');
 });
 
-server.get('/', ( req, res ) => {
-    res.status( 200 )
-        .header({ 'Content-Type': 'text/html'})
-        .send( readFileSync( __dirname + '/index.html') );
-})
-server.get('/about', ( req, res ) => {
-    res.status( 200 )
-        .sendFile( __dirname + '/about.html' );
-})
+server.use('/', express.static(__dirname + '/') );
+server.use('/about', express.static(__dirname + '/about.html') );
+server.all('*', express.static(__dirname + '/error.html') );
+// server.get('/', ( req, res ) => {
+//     res.status( 200 )
+//         .header({ 'Content-Type': 'text/html'})
+//         .send( readFileSync( __dirname + '/index.html') );
+// })
+// server.get('/about', ( req, res ) => {
+//     res.status( 200 )
+//         .sendFile( __dirname + '/about.html' );
+// })
 
-server.all('*', ( req, res ) => {
-    res.status( 404 )
-        .send( readFileSync( __dirname + '/error.html'));
-})
+// server.all('*', ( req, res ) => {
+//     res.status( 404 )
+//         .send( readFileSync( __dirname + '/error.html'));
+// })
